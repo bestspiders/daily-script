98ceb95e1ae76507b2e7c02f711918ee9e496a9f
bestspiders <1964392697@qq.com>
2018-09-21 09:57:46 +0800

    Signed-off-by: bestspiders <1964392697@qq.com>

---
+++ Readme.md
@@ -0,0 +1,58 @@
+#bound_symbol_unzip.py <br>
+**Python ENV:**2.7+ <br>
+针对日期格式清理:catalina.2018-09-21.out <br>
+使用例子: <br>
+```
+python bound_symbol_unzip.py -t 3 -l 11 -p /usr/local/tomcat/logs -d 1
+```
+
+-t : 需要压缩的包天数（距离当天的时间）<br>
+-l : 压缩包保留天数 (距离今天的时间) <br>
+-d : 源文件保留天数 (距离今天的时间) <br>
+-p : 压缩的目录 <br>
+
+#old_bound_symbol_unzip.py <br>
+**Python ENV:** 2.4+ <br>
+同上，但是可以适用比较老的系统 <br>
+参数同上解释<br>
+#no_bound_symbol_unzip.py <br>
+**Python ENV:** 2.7+ <br>
+针对日期格式清理:catalina.20180921.out <br>
+使用例子: <br>
+```
+python bound_symbol_unzip.py -t 3 -l 11 -p /usr/local/tomcat/logs -d 1
+```
+参数同上解释<br>
+
+#send_log_zabbix.py <br>
+针对tomcat当天日志关键词检查报警<br>
+
+使用例子<br>
+**使用前需要在/tmp/创建tomcat_check.log**
+**写入输出化参数，如下**
+```
+[check_log]
+last_line = 1
+last_time = 2018-09-21
+```
+需要把1替换成需要从哪行检查
+2018-09-21替换成当天日期
+```
+python send_log_zabbix.py -p /usr/local/tomcat/logs -t 300 -c jdbc,exception -k jdbc_key,exception_key -r 10051 -s 172.16.1.1 -l host_name
+参数
+```
+  -h, --help            show this help message and exit
+  -p PATH, --path=PATH  tomcat logs path
+  -t WAIT_TIME, --wait_time=WAIT_TIME
+                        loop wait time
+  -c CHECK_LIST, --check_list=CHECK_LIST
+                        check key list
+  -k KEY_LIST, --key_list=KEY_LIST
+                        server key list
+  -r PORT, --port=PORT  server port
+  -s SERVER_HOST, --server_host=SERVER_HOST
+                        server host ip
+  -l HOST_NAME, --host_name=HOST_NAME
+                        local host name
+
+```
---
+++ no_bound_symbol_unzip.py
@@ -0,0 +1,37 @@
+#-*- coding:utf-8 -*-
+import os,tarfile,re,time,datetime,shutil
+import argparse
+parser=argparse.ArgumentParser()
+parser.add_argument("-t","--tar",help="gzip time")
+parser.add_argument("-d","--delete",help="raw logs delete time")
+parser.add_argument("-p","--path",help="logs path")
+parser.add_argument("-l","--local_tar_del",help="tar package delete")
+args=parser.parse_args()
+log_path=args.path
+all_file=os.listdir(log_path)
+#today_time=time.strftime("%Y-%m-%d")
+today=datetime.date.today()
+time_delete=[(today-datetime.timedelta(days=add_time)).strftime('%y%m%d') for add_time in range(int(args.delete),365)]
+time_tar=[(today-datetime.timedelta(days=add_time)).strftime('%y%m%d') for add_time in range(1,int(args.tar)+1)]
+time_tar_delete=[(today-datetime.timedelta(days=add_time)).strftime('%y%m%d') for add_time in range(int(args.local_tar_del),365)]
+for every_day in time_tar_delete:
+    for every_file in all_file:
+        if re.search(every_day,every_file):#如果文件路径含时间
+            if re.search('\.tar\.gz',every_file):#如果文件路径包含.tar.gz
+                os.remove(os.path.join(log_path,every_file))
+
+for tar_day in time_tar:
+    for every_file in all_file:
+        if re.search(tar_day,every_file):
+#           if  not os.path.exists(os.path.join(log_path,every_file+'.tar.gz')):
+            if not re.search('\.tar\.gz',every_file):
+                if not os.path.exists(os.path.join(log_path,every_file+'.tar.gz')):
+                    print(os.path.join(log_path,every_file)+'.tar.gz')
+                    t=tarfile.open(os.path.join(log_path,every_file)+'.tar.gz','w:gz')
+                    t.add(os.path.join(log_path,every_file))
+                    t.close()
+for every_day in time_delete:
+    for every_file in all_file:
+        if re.search(every_day,every_file):#如果文件路径含时间
+            if not re.search('\.tar\.gz',every_file):#如果文件路径不包含.tar.gz
+                os.remove(os.path.join(log_path,every_file))
